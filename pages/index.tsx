e467076208440c317d5832d2214215610633d6e5
Raphael <raphael.t.ruban@gmail.com>
2020-07-23 17:00:46 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Roundnet-Stats</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Roundnet-Statistics!
         </h1>
 
         <p className="description">
@@ -49,14 +49,7 @@ export default function Home() {
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        Created by Raphael
       </footer>
 
       <style jsx>{`
