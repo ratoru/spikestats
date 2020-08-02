3ae60f15e63e677c13f0c916d2ab478dfe0cad88
Raphael <raphael.t.ruban@gmail.com>
2020-08-02 18:31:46 +0200

    Fixed bottom navbar margin

--- components/NavStats.tsx
+++ components/NavStats.tsx
@@ -32,7 +32,11 @@ function TabPanel(props: TabPanelProps) {
       aria-labelledby={`full-width-tab-${index}`}
       {...other}
     >
-      {value === index && <Box p={3}>{children}</Box>}
+      {value === index && (
+        <Box p={3} marginBottom={8}>
+          {children}
+        </Box>
+      )}
     </div>
   );
 }
