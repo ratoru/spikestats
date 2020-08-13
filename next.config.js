f30dc242ed37adf09077cd06a9b930f6d3c1c9ef
Raphael <raphael.t.ruban@gmail.com>
2020-08-13 19:51:52 +0200

    renamed back-end

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// Have to add nextjs transpiler
-// https://github.com/amcharts/amcharts4/issues/104
-const withTM = require("next-transpile-modules")([
-  "@amcharts/amcharts4/core",
-  "@amcharts/amcharts4/charts",
-  "@amcharts/amcharts4/themes/animated",
-]); // pass the modules you would like to see transpiled
-
-module.exports = withTM();
