8ba669decb27e72e41b68027e0a766200cf4088a
Raphael <raphael.t.ruban@gmail.com>
2020-09-11 12:21:02 +0200

    Updated about page

--- front-end/src/pages/about.tsx
+++ front-end/src/pages/about.tsx
@@ -15,13 +15,12 @@ export default function About() {
         </Grid>
         <Grid item container spacing={2} justify="center" alignItems="center">
           <Grid item xs={12}>
+            <Typography variant="h5">Welcome</Typography>
             <Typography>
               Hi! This little web app allows you to track your 'Roundnet' games.
               I hope you enjoy it. Please never save any sensitive information
               on this website.
             </Typography>
-          </Grid>
-          <Grid item xs={12}>
             <Typography>
               Furthermore, this is a programming project I am working on in my
               free time. You can find the code{" "}
@@ -30,6 +29,23 @@ export default function About() {
             </Typography>
           </Grid>
           <Grid item xs={12}>
+            <Typography variant="h5">Constraints</Typography>
+            <Typography>
+              You'll be logged out every two hours. This web app uses Vercel's
+              and Heroku's free plan. This means 1) if no one used the website
+              for a long time it will take significantly longer to load. 2) my
+              database has a very limited amount of storage. If the database is
+              full this website won't work anymore. If you'd like to contribute,
+              you're more than welcome to reach out to me.
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h5">Privacy</Typography>
+            <Typography>
+              Never store any sensitive information on this website, i.e. don't
+              use your email as a username! I did my best to follow common
+              security guidelines, but you shouldn't rely on that.
+            </Typography>
             <Typography>
               Please remember your username! There is no way of getting your
               account back if you lose it. I did not use emails because I didn't
@@ -37,6 +53,7 @@ export default function About() {
             </Typography>
           </Grid>
           <Grid item xs={12}>
+            <Typography variant="h5">Contributions</Typography>
             <Typography>
               Special thanks to Christina Joo for helping me with the logo
               design.
